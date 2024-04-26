2f4f01b59afc57dcbc2bab942d951a9f7f651cd6
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-26 18:37:09 -0300

    Renamed namespace

---
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Input/InputHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.InputReader
+{
+    [CreateAssetMenu(menuName = "Gameplay/Input/Input Reader", fileName = "NewInputHandler")]
+    public class InputHandler : ScriptableObject
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
