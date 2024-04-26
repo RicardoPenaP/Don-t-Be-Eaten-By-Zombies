095f011d26e55eb82171464f62e2174f89a457c8
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-26 20:32:17 -0300

    Updated InputReader class
    Implemented attack with aim stick logic

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Input/InputReader.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Input/InputReader.cs
@@ -8,6 +8,11 @@ namespace Gameplay.Input
     [CreateAssetMenu(menuName = "Gameplay/Input/Input Reader", fileName = "NewInputReader")]
     public class InputReader : ScriptableObject, IPlayerActions
     {
+        [Header("Input Reader")]
+        [Header("Settings")]
+        [SerializeField] private bool attackWithAimStick = true;
+        [SerializeField, Range(0f, 1f)] private float aimDeathzoneForAttack;
+
         public event Action<Vector2> OnAimInputUpdated;
         public event Action<bool> OnAttackInputUpdated;
         public event Action OnChangeWeaponInputUpdated;
@@ -18,7 +23,7 @@ namespace Gameplay.Input
 
         private void OnEnable()
         {
-            Init();           
+            Init();
         }
 
         private void Init()
@@ -33,16 +38,22 @@ namespace Gameplay.Input
         }
 
         public void OnAim(InputAction.CallbackContext context)
-        {            
+        {
             Vector2 aimRawInput = context.ReadValue<Vector2>();
             OnAimInputUpdated?.Invoke(aimRawInput);
+
+            if (!attackWithAimStick)
+            {
+                return;
+            }
+            OnAttackInputUpdated?.Invoke(aimRawInput.magnitude >= aimDeathzoneForAttack);            
         }
 
         public void OnAttack(InputAction.CallbackContext context)
         {
             bool state = false;
             switch (context.phase)
-            {                
+            {
                 case InputActionPhase.Performed:
                     state = true;
                     break;
