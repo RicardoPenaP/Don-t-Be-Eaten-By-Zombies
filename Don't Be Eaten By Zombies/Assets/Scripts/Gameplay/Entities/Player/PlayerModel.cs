358b246b5751084773f60a50d1e6978f6ecaf12f
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-27 02:20:30 -0300

    Updated PlayerModel class
    Implemented MoveTowards method logic

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Player/PlayerModel.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Player/PlayerModel.cs
@@ -6,12 +6,17 @@ namespace Gameplay.Entities.Player
     public class PlayerModel : MonoBehaviour, IMovementModel
     {
         [Header("Player Model")]
+
         [Header("References")]
         [SerializeField] private Rigidbody2D playerRigidbody2D;
 
+        [Header("Settings")]
+        [SerializeField] private MovementSettings movementSettings;
+
         public void MoveTowards(Vector2 movementDirection)
         {
-            throw new System.NotImplementedException();
+            //Only use forward speed for testing
+            playerRigidbody2D.velocity = movementSettings.ForwardSpeed * movementDirection;
         }
 
         public void StopMovement()
