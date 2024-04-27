91b29be4d148c07fc3578937ec7d0109408e657a
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-27 02:17:21 -0300

    Created MovementSettings class

---
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Movement/MovementSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Entities.Common.Movement
+{
+    [CreateAssetMenu(fileName = "MovementSettings", menuName = "Gameplay/Entities/Common/Movement/MovementSettings")]
+    public class MovementSettings : ScriptableObject
+    {
+        [Header("Movement Settings")]
+        [SerializeField] private float forwardSpeed = 5f;
+        [SerializeField] private float backwardSpeed = 5f;
+
+        public float ForwardSpeed => forwardSpeed;
+        public float BackwardSpeed => backwardSpeed;
+
+    }
+}
