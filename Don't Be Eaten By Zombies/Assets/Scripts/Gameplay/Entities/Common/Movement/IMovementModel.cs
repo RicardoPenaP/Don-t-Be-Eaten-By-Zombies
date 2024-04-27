b19103657b77553775e30bf043849031a8983a2a
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-27 01:56:29 -0300

    Updated IMovementModel interface
    Defined MoveTowards method

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Movement/IMovementModel.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Movement/IMovementModel.cs
@@ -1,7 +1,9 @@
-﻿namespace Gameplay.Entities.Common.Movement
+﻿using UnityEngine;
+
+namespace Gameplay.Entities.Common.Movement
 {
     public interface IMovementModel 
     {
-
+        public void MoveTowards(Vector2 movementDirection);
     }
 }
