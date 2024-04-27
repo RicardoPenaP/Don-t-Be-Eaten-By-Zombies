a440bc33efddf5f0882d202abcfb3ed51e2d4247
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-27 01:47:48 -0300

    Updated MovementController class
    Updated class constructor and defined new fields

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Movement/MovementController.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Movement/MovementController.cs
@@ -1,11 +1,15 @@
 namespace Gameplay.Entities.Common.Movement
 {
-    public class MovementController 
+    public class MovementController
     {
-        public MovementController()
-        {
+        private readonly IMovementView movementView;
+        private readonly IMovementModel movementModel;
 
+        public MovementController(IMovementView movementView, IMovementModel movementModel)
+        {
+            this.movementView = movementView;
+            this.movementModel = movementModel;
         }
-        
+
     }
 }
