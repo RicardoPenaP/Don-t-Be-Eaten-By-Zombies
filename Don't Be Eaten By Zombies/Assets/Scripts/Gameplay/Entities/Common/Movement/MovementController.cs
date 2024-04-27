ab3b09a2db826db7f7391d45ddf1bd9741c7c0c4
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-27 01:39:35 -0300

    Created MovementController class

---
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Movement/MovementController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.Entities.Common.Movement
+{
+    public class MovementController 
+    {
+        
+    }
+}
