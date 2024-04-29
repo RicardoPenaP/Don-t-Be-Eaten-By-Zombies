403a65af7b413c780fcd51990012f84a0f179d40
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-29 15:41:42 -0300

    Updated IHealthModel interface
    Defined events and method for health handling

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Health/IHealthModel.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Health/IHealthModel.cs
@@ -1,6 +1,13 @@
-﻿namespace Gameplay.Entities.Common.Health
+﻿using System;
+
+namespace Gameplay.Entities.Common.Health
 {
     public interface IHealthModel 
     {
+        public event Action<int> OnHealthModified;
+        public event Action OnHealthReachedZero;
+
+        public void ReduceHealth(int amount);
+        public void RestoreHealth(int amount);
     }
 }
