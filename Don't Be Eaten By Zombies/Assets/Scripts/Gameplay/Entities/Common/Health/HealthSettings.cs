5eae526551ece224ffa39bd904b43f4c46f5f10a
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-29 17:01:37 -0300

    Updated HealthSettings class

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Health/HealthSettings.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Health/HealthSettings.cs
@@ -5,6 +5,9 @@ namespace Gameplay.Entities.Common.Health
     [CreateAssetMenu(fileName = "NewHealthSettings", menuName = "Gameplay/Entities/Common/Health/Health Settings")]
     public class HealthSettings : ScriptableObject
     {
+        [Header("Health Settings")]
+        [SerializeField] private int defaulMaxHealth = 100;
 
+        public int DefaulMaxHealth => defaulMaxHealth;
     }
 }
