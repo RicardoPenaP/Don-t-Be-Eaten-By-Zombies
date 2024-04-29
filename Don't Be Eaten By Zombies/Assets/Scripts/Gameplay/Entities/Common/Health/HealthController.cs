982bd3f92c2788b20888dba74fc0e60872466dde
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-29 14:47:44 -0300

    Created IHealthView interface

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Health/HealthController.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Health/HealthController.cs
@@ -1,21 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Gameplay.Entities.Common
+namespace Gameplay.Entities.Common.Health
 {
-    public class HealthController : MonoBehaviour
+    public class HealthController
     {
-        // Start is called before the first frame update
-        void Start()
-        {
         
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
---
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Health/IHealthView.cs
@@ -0,0 +1,7 @@
+﻿namespace Gameplay.Entities.Common.Health
+{
+    public interface IHealthView
+    {
+
+    }
+}
