40aa86c384d040076538da6119e21a7d8d01ba42
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-29 14:13:50 -0300

    Updated AimController class
    Defined constructor and initialization methods, and implemented IDisposable interface

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Aim/AimController.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Aim/AimController.cs
@@ -1,7 +1,32 @@
+using System;
+
 namespace Gameplay.Entities.Common.Aim
 {
-    public class AimController 
+    public class AimController : IDisposable
     {
-       
+        private readonly IAimView aimView;
+        private readonly IAimModel aimModel;
+
+        public AimController(IAimView aimView, IAimModel aimModel)
+        {
+            this.aimView = aimView;
+            this.aimModel = aimModel;
+            Init();
+        }
+
+        private void Init()
+        {
+
+        }
+
+        private void Deinit()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            Deinit();
+        }
     }
 }
