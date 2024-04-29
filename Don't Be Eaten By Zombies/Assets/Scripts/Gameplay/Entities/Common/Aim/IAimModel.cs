f253e534bcf9455b93f96a4ab45c2912803cf18c
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-29 14:33:44 -0300

    Updated IAimModel interface

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Aim/IAimModel.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Aim/IAimModel.cs
@@ -1,7 +1,13 @@
-﻿namespace Gameplay.Entities.Common.Aim
+﻿using UnityEngine;
+
+namespace Gameplay.Entities.Common.Aim
 {
     public interface IAimModel
     {
-        
+        /// <summary>
+        /// Aims towards a normalized direction used to set the transform.Right vector.
+        /// </summary>
+        /// <param name="aimDirection">Normalized vector.</param>       
+        public void AimTowards(Vector2 aimDirection);
     }
 }
