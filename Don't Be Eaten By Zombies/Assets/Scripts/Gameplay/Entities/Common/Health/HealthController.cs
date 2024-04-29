c854d2e05ad0f7516a010103c99a4caca4b5c64f
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-29 14:56:49 -0300

    Updated HealthController class
    Implemented IDisposable interface and defined Init and Deinit methods

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Health/HealthController.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Health/HealthController.cs
@@ -1,7 +1,32 @@
+using System;
+
 namespace Gameplay.Entities.Common.Health
 {
-    public class HealthController
+    public class HealthController : IDisposable
     {
-        
+        private readonly IHealthView healthView;
+        private readonly IHealthModel healthModel;
+
+        public HealthController(IHealthView healthView, IHealthModel healthModel)
+        {
+            this.healthView = healthView;
+            this.healthModel = healthModel;
+            Init();
+        }
+
+        private void Init()
+        {
+
+        }
+
+        private void Deinit()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            Deinit();
+        }
     }
 }
