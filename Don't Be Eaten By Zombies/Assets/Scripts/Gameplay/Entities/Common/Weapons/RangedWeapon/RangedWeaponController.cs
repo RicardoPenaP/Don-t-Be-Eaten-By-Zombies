1ecd6f6af1890c2b1fc1c9c2992d1f3da7b697fe
RicardoPenaP <ricardopena241197@gmail.com>
2024-04-30 19:24:02 -0300

    Updated RangedWeaponController class

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/RangedWeapon/RangedWeaponController.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/RangedWeapon/RangedWeaponController.cs
@@ -4,6 +4,11 @@ namespace Gameplay.Entities.Common.Weapons.RangedWeapons
 {
     public class RangedWeaponController : MonoBehaviour
     {
-        
+        [Header("Ranged Weapon Controller")]
+        [Header("References")]
+        [SerializeField] private RangedWeaponView rangedWeaponView;
+        [SerializeField] private RangedWeaponModel rangedWeaponModel;
+
+
     }
 }
