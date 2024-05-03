4fd7848ecd1aa3a4995c37f101b10683559119a0
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-02 22:24:09 -0300

    Created WeaponReloadController class

---
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/Common/Reload/WeaponReloadController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Gameplay.Entities.Common.Weapons.Common.Reload
+{
+    public class WeaponReloadController : IDisposable
+    {
+        private void Init()
+        {
+
+        }
+
+        private void Deinit()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            Deinit();
+        }
+
+    }
+}
