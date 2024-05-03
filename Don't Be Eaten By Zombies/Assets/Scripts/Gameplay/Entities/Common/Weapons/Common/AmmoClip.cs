46a0447757408e1132fee8d17762c33ad8f65cb1
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-02 23:01:56 -0300

    Created AmmoClip class

---
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/Common/AmmoClip.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Entities.Common.Weapons.Common
+{
+    public class AmmoClip
+    {
+        private readonly int maxAmmo;
+        private int currentAmmo;
+
+        public int MaxAmmo => maxAmmo;
+
+        public int CurrentAmmo => currentAmmo;
+
+        public AmmoClip(int clipSize)
+        {
+            maxAmmo = clipSize;
+            ReloadAmmo(maxAmmo);
+        }
+
+        public void ReloadAmmo(int amount)
+        {
+            currentAmmo = Mathf.Clamp(currentAmmo + amount, 0, maxAmmo);
+        }
+
+        public bool TryTakeAmmo()
+        {
+            if (currentAmmo <= 0)
+            {
+                return false;
+            }
+            currentAmmo--;
+            return true;
+        }
+    }
+}
