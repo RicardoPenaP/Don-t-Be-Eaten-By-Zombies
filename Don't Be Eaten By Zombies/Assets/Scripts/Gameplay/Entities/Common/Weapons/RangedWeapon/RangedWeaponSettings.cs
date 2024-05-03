07d25e6de3ccc07e6ca30c1aa91f150cffe389ca
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-02 21:27:21 -0300

    Created RangedWeaponSettings class

---
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/RangedWeapon/RangedWeaponSettings.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Entities.Common.Weapons.RangedWeapon
+{
+    [CreateAssetMenu(fileName = "NewRangedWeaponSettings", menuName = "Gameplay/Entities/Common/Weapons/Ranged Weapon Settings")]
+    public class RangedWeaponSettings : ScriptableObject
+    {
+        [Header("Ranged Weapons Settings")]
+        [SerializeField] private int damage;
+        [SerializeField] private int clipSize;
+        [SerializeField] private float reloadTime;
+        [SerializeField] private float fireRate;
+
+        public int Damage => damage;
+        public int ClipSize => clipSize;
+        public float ReloadTime => reloadTime;
+        public float FireRate => fireRate;
+    }
+}
