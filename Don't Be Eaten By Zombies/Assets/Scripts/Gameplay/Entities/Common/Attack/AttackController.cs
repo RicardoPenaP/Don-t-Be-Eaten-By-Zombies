302731aff0e7bf17e9ab8fd8912c3c3b162a56ed
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-03 01:11:16 -0300

    Created AttackController class

---
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Attack/AttackController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Gameplay.Entities.Common.Attack
+{
+    public class AttackController : IDisposable
+    {
+        private void Init()
+        {
+
+        }
+
+        private void Deinit()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            Deinit();
+        }
+    }
+}
