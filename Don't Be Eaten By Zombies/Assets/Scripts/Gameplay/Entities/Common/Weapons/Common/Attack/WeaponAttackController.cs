94e101c656c4bfc1c7f18e2d94922964f385dbe6
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-02 22:03:00 -0300

    Updated WeaponAttackController class
    Defined constructor, Init and Deinit methods

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/Common/Attack/WeaponAttackController.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/Common/Attack/WeaponAttackController.cs
@@ -1,7 +1,32 @@
-﻿namespace Gameplay.Entities.Common.Weapons.Common.Attack
-{
-    public class WeaponAttackController 
-    {
+﻿using System;
 
+namespace Gameplay.Entities.Common.Weapons.Common.Attack
+{
+    public class WeaponAttackController : IDisposable
+    {
+        private readonly IWeaponAttackView weaponAttackView;
+        private readonly IWeaponAttackModel weaponAttackModel;
+
+        public WeaponAttackController(IWeaponAttackView weaponAttackView, IWeaponAttackModel weaponAttackModel)
+        {
+            this.weaponAttackView = weaponAttackView;
+            this.weaponAttackModel = weaponAttackModel;
+            Init();
+        }
+
+        private void Init()
+        {
+
+        }
+
+        private void Deinit()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            Deinit();
+        }
     }
 }
