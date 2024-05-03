18f8c9c541074b124b8d7a94927d9dba379071f8
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-02 22:34:43 -0300

    Updated WeaponReloadController class
    Defined constructor and Reload method

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/Common/Reload/WeaponReloadController.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/Common/Reload/WeaponReloadController.cs
@@ -4,6 +4,16 @@ namespace Gameplay.Entities.Common.Weapons.Common.Reload
 {
     public class WeaponReloadController : IDisposable
     {
+        private readonly IWeaponReloadView weaponReloadView;
+        private readonly IWeaponReloadModel weaponReloadModel;
+
+        public WeaponReloadController(IWeaponReloadView weaponReloadView, IWeaponReloadModel weaponReloadModel)
+        {
+            this.weaponReloadView = weaponReloadView;
+            this.weaponReloadModel = weaponReloadModel;
+            Init();
+        }
+
         private void Init()
         {
 
