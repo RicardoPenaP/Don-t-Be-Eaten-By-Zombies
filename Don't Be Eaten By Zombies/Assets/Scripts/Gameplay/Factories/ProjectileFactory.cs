1553ae7f9cce112534b9a9f1834969403788ddde
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-06 19:54:55 -0300

    Updated ProjectileFactory class

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Factories/ProjectileFactory.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Factories/ProjectileFactory.cs
@@ -4,6 +4,29 @@ namespace Gameplay.Factories
 {
     public class ProjectileFactory : MonoBehaviour
     {
-        
+        private static ProjectileFactory instance;
+
+        public static ProjectileFactory Instance => instance;
+
+        [Header("Projectile Factory")]
+        [Header("References")]
+        [SerializeField] GameObject projectiles;
+
+        private void Awake()
+        {
+            if (instance is not null && !instance.Equals(this))
+            {
+                Destroy(this);
+            }
+            else
+            {
+                instance = this;                
+            }
+        }
+
+        public GameObject SpawnProjectile(Vector3 spawnPosition)
+        {
+            return null;
+        }
     }
 }
