279c0fed82c086437b887ee67fb3a35538f03890
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-06 19:59:11 -0300

    Created SpawnableProjectile class

---
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Factories/Projectile/SpawnableProjectile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Gameplay.Factories.Assets.Scripts.Gameplay.Factories.Projectile
+{
+    public class SpawnableProjectile : MonoBehaviour
+    {
+        //[Header("Spawnable Projectile")]
+        //[SerializeField] private string namess;
+    }
+}
