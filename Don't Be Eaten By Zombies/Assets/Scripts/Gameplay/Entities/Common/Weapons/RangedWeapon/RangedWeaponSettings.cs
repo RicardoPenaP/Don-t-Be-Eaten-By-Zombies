7fd2f10ea9027bf4d0dec02dfdc3a3e65939501e
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-06 19:40:37 -0300

    Updated RangedWeaponSettings class
    Defined class field and property

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/RangedWeapon/RangedWeaponSettings.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/RangedWeapon/RangedWeaponSettings.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Gameplay.Entities.Common.Weapons.RangedWeapon.Projectiles;
+using UnityEngine;
 
 namespace Gameplay.Entities.Common.Weapons.RangedWeapon
 {
@@ -6,14 +7,17 @@ namespace Gameplay.Entities.Common.Weapons.RangedWeapon
     public class RangedWeaponSettings : ScriptableObject
     {
         [Header("Ranged Weapons Settings")]
+        [SerializeField] private ProjectileSettings projectileSettings;
         [SerializeField] private int damage;
         [SerializeField] private int clipSize;
         [SerializeField] private float reloadTime;
         [SerializeField] private float fireRate;
 
+        public ProjectileSettings ProjectileSettings => projectileSettings;
         public int Damage => damage;
         public int ClipSize => clipSize;
         public float ReloadTime => reloadTime;
         public float FireRate => fireRate;
+
     }
 }
