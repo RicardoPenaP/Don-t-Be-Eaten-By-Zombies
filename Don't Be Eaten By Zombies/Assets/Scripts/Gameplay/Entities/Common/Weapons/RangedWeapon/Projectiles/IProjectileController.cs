e3cddad45c6b4390e776415027000774eb48e8c9
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-06 17:23:50 -0300

    Updated IProjectileController interface
    Defined method

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/RangedWeapon/Projectiles/IProjectileController.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/RangedWeapon/Projectiles/IProjectileController.cs
@@ -2,6 +2,15 @@ namespace Gameplay.Entities.Common.Weapons.RangedWeapons.Projectiles
 {
     public interface IProjectileController
     {
+        public void SetProjectileData()
+        {
+
+        }
+
+        public void StarProjectileMovement()
+        {
+
+        }
        
     }
 }
