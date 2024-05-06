f6ff60bb68bf55be77cb7e38a03a6934456f811a
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-06 19:02:56 -0300

    Updated BulletController class
    Defined view and model fields

--- Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/RangedWeapon/Projectiles/Bullets/BulletController.cs
+++ Don't Be Eaten By Zombies/Assets/Scripts/Gameplay/Entities/Common/Weapons/RangedWeapon/Projectiles/Bullets/BulletController.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 namespace Gameplay.Entities.Common.Weapons.RangedWeapon.Projectiles.Bullets
 {
     public class BulletController : MonoBehaviour, IProjectileController
-    {        
+    {
+        [Header("Bullet Controller")]
+        [Header("References")]
+        [SerializeField] BulletView bulletView;
+        [SerializeField] BulletModel bulletModel;
+
+
+
         public void SetProjectileData()
         {
             throw new System.NotImplementedException();
